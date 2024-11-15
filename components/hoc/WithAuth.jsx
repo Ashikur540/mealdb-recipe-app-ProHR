a2ec1593cc76860d935464ff56159b81d05622c5
Ashikur Rahman <ashikur35-540@diu.edu.bd>
2024-11-15 12:21:51 +0600

    feat: Added env properly

--- components/hoc/WithAuth.jsx
+++ components/hoc/WithAuth.jsx
@@ -1,15 +1,21 @@
 "use client"
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import { useAuth } from '@/providers/AuthProvider'
 
 const WithAuth = (WrappedComponent) => {
 
   return function AuthComponent(props) {
-    const { user} = useAuth()
+    const { user } = useAuth()
     const router = useRouter()
 
+    // Don't render the wrapped component if the user is authenticated
+    useEffect(() => {
+      if (user) {
+        router.push("/")
+      }
+    }, [user, router])
     if (user) {
-      router.push("/")
       return null
     }
 
